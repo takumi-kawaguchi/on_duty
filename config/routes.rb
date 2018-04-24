d01bd5cf21a1042095647c1df38c5a67a8a62b9d
takumi-kawaguchi <tcgds4@gmail.com>
2018-04-25 02:26:05 +0900

    未ログインユーザーは強制的にログイン・サインアップページに飛ばす

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/show'
-  devise_for :users 
-
+  # devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
